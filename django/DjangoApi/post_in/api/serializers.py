0fd60a87a43c57d6240dea9ba8899f51a248892e
StimDoker <73744420+StimDoker@users.noreply.github.com>
2021-09-02 16:16:35 +0300

    add

---
+++ django/DjangoApi/post_in/api/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework.serializers import (SerializerMethodField,IntegerField, CharField,HyperlinkedIdentityField, Serializer, ModelSerializer)
+from notes.models import Note
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        queryset = model.objects.all()
+        fields = ('id','email','password','name','admin')
+        extra_kwargs = {'password':{'write_only': True}}#только для юзера
+
+    def create(self,validated_data):
+        password = validated_data.pop('password','')
+        user = self.Meta.model(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance,validated_data):
+        instance.set_passwords(validated_data.pop('password',''))
+        return super().update(instance, validated_data)
+
+
+
+class NoteSerializer(ModelSerializer): #содержит все тоже самое что снизу
+    author = SerializerMethodField(read_only=True)
+
+    def get_author(self,obj):
+        return obj.author.email # или str(obj.author.id)
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+
+class ThinNoteSerializer(ModelSerializer):
+    url = HyperlinkedIdentityField(view_name='notes-detail')
+    class Meta:
+        model = Note
+        fields = ('id','title','url')
+
+""" class NoteSerializer(Serializer):
+    id = IntegerField(read_only=True)
+    title = CharField(required=True, max_length = 255)
+    text = CharField(required=False, allow_blank=True)
+
+    def create(self, validated_data):
+        return Note.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.text = validated_data.get('text', instance.text)
+        instance.save()
+        return instance """
+
